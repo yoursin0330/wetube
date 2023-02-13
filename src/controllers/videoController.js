6f11bddc1a324d361b74e01c4020d1880053b50e
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2023-02-13 11:52:53 +0900

    Mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,45 @@
+let videos=[
+    {
+        title: "First Video",
+        rating: 5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 3,
+    }
+];
+
 //global
-export const trending =(req,res)=>res.render("home",{pageTitle: "Home"});
+export const trending =(req,res)=>{
+    // const videos=[1,2,3,4,5,6,7,8,9,10]
+    
+    
+    res.render("home",{pageTitle: "Home", videos});
+}
 export const search=(req,res)=>res.send("Search")
 
 //video
-export const see =(req,res)=>res.render("watch")
-
+export const see =(req,res)=>{
+    const {id} = req.params;
+    const video = videos[id-1];
+    return res.render("watch",{pageTitle:`Watching ${video.title}`})
+}
 export const edit = (req,res)=>res.send("Edit")
 export const deleteVideo = (req,res) =>{
     console.log(req.params)
